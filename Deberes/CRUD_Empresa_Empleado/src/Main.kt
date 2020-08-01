e6c3d0835efc1c40d39c68b911f84f78953a7855
Hugo <hugo.moncayo@epn.edu.ec>
2020-08-01 15:01:57 -0500

    Hasta crear Menús
    Creación clases funciones básicas y crear menús

--- Deberes/CRUD_Empresa_Empleado/src/Main.kt
+++ Deberes/CRUD_Empresa_Empleado/src/Main.kt
@@ -1,9 +1,12 @@
 
 fun main(args:Array<String>) {
 
-    val entrenadores = ArrayList<Empresas>()
-    val pokemones = ArrayList<Empleado>()
-
+    val empresas = ArrayList<Empresas>()
+    val empleados = ArrayList<Empleado>()
+    //println("prueba de correr main
+    // como clase luego de configuracion")
+    while (true)
+        menu(empresas,empleados)
 
 }
 class Empresas(
@@ -14,7 +17,7 @@ class Empresas(
     var capitalEmpresa:Float
 ){
     override fun toString(): String {
-        return "Empresa(Empresa='$nombreEmpresa', " +
+        return "Empresa(Nombre Empresa='$nombreEmpresa', " +
                 "Direccion='$direccionEmpresa', " +
                 "Telefono Empresa='$telefonEmpresa', " +
                 "Estado Empresa=$estadoEmpresa, " +
@@ -29,10 +32,110 @@ class Empleado(
     var departamentoEmpleado:String
 ){
     override fun toString(): String {
-        return "Pokemon(Empleado='$nombresEmpleado', " +
+        return "Empleado (Nombre Empleado='$nombresEmpleado', " +
                 "Salario=$salarioEmpleado, " +
                 "edadEmpleado=$edadEmpleado, " +
                 "Estado Empleado=$estadoEmpleado, " +
                 "Departamento='$departamentoEmpleado')"
     }
+}
+
+//Menús
+fun menu(empresas: ArrayList<Empresas>, empleados: ArrayList<Empleado>) {
+    var texto: String
+    texto = "********************************************" + "\n"
+    texto += "-          Menú Principal            " + "\n"
+    texto += "1. Menú Empresa        " + "\n"
+    texto += "2. Menú Empleados  " + "\n"
+    texto += "3. Salir                           " + "\n"
+    texto += "********************************************" + "\n"
+    println(texto)
+
+    var numeroMenu: Int
+    while (true) {
+        try {
+            numeroMenu = readLine().toString().toInt()
+            if (numeroMenu > 6 || numeroMenu < 1) {
+                print("Ingrese un número entre 1 a 6 ")
+            } else {
+                break
+            }
+        } catch (e: NumberFormatException) {
+            print("Ingrese un número ")
+        }
+    }
+
+    when (numeroMenu) {
+        1 ->
+            {
+                while (menuEmpresa(empresas, empleados)) {
+                }
+            }
+        2 ->
+            {
+                while (menuEmpleado(empresas, empleados)) {
+                }
+            }
+        3 -> System.exit(0)
+    }
+
+
+}
+
+fun menuEmpresa(empresas: ArrayList<Empresas>, empleados: ArrayList<Empleado>): Boolean {
+    var estado = true
+    var texto: String
+    texto = "********************************************" + "\n"
+    texto += "-       MENÚ EMPRESAS       -" + "\n"
+    texto += "- 1. Ingresar Empresa (C)    -" + "\n"
+    texto += "- 2. Mostrar Empresa (R) -" + "\n"
+    texto += "- 3. Modificar Empresa (U)          -" + "\n"
+    texto += "- 4. Eliminar Empresa  (D)         -" + "\n"
+    texto += "- 5. Regresar         -" + "\n"
+    texto += "********************************************" + "\n"
+    println(texto)
+    var opcion: Int
+    while (true) {
+        try {
+            opcion = readLine().toString().toInt()
+            if (opcion > 5 || opcion < 1) {
+                print("ingrese un valor correcto")
+            } else {
+                break
+            }
+        } catch (e: NumberFormatException) {
+            print("Ingrese un valor valido")
+        }
+    }
+    when (opcion) {
+        1 -> crearEmpresa(empresas)
+        2 -> mostrarEmpresa()
+        3 -> modificarEmpresa()
+        4 -> eliminarEmpresa()
+        5 -> estado = false
+    }
+    return estado
+}
+
+fun crearEmpresa(empresas : ArrayList<Empresas>){
+    print("Ingrese nombre de la Empresa: ")
+    val nombre = readLine().toString()
+    print("Ingrese Dirección de la Empresa: ")
+    val direccion = readLine().toString()
+    print("Ingrese teléfono de la Empresa: ")
+    val telefono = readLine().toString().toInt()
+    print("La Empresa está activa? (true o false) ")
+    val estado = readLine()!!.toBoolean()
+    print("Ingrese el Capital de la Empresa: ")
+    val capital = readLine().toString().toFloat()
+    empresas.add( Empresas(nombre,direccion,telefono,estado,capital))
+}
+fun mostrarEmpresa(){
+
+}
+fun modificarEmpresa(){
+
+}
+fun eliminarEmpresa (){
+
 }
